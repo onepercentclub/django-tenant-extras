3e5698ada8968752ac47b7e6f1b4a1e2d5d2b6cb
rollick <mark@onepercentclub.com>
2015-02-17 15:35:30 +0200

    update manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 import tenant_extras
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
