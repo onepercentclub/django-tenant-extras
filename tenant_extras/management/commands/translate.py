9a26e50b8ea3de35cbdd67edf93a77c121880981
rollick <mark@onepercentclub.com>
2015-03-03 17:20:00 +0100

    rename pocmd argument in translate command

--- tenant_extras/management/commands/translate.py
+++ tenant_extras/management/commands/translate.py
@@ -24,7 +24,7 @@ class Command(BaseCommand):
                     help='locale(s) to process (e.g. de_AT). Default is to process all. Can be used multiple times.'),
             make_option('--compile', '-c', dest='compile', action='store_true', default=False,
                     help='compile the .po to .mo files.'),
-            make_option('--pocmd', '-p', dest='pocmd', default='makepo', 
+            make_option('--pocmd', '-d', dest='pocmd', default='makepo', 
                     help='alternative command to generate po files'),
         )
 
