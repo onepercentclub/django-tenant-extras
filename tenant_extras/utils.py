78ac2ef24706376491afb8e79dd6cdff23dd4621
rollick <mark@onepercentclub.com>
2015-05-06 17:13:45 +0200

    switch to class based context manager for tenant language

--- tenant_extras/utils.py
+++ tenant_extras/utils.py
@@ -47,23 +47,43 @@ def update_tenant_site(tenant, name, domain):
     connection.set_schema_to_public()
 
 
-@contextmanager
-def tenant_language(language):
-    """Context manager that changes the current translation language for
-    all code inside the following block to the correct tenant specific language
-    """
-    from .middleware import tenant_translation
+class TenantLanguage():
 
-    if language:
-        prev = translation.get_language()
+    def __init__(self, language):
+        self.language = language
+        self.prev = translation.get_language()
 
+    def __enter__(self):
+        from .middleware import tenant_translation
+
+        tenant_name = connection.tenant.client_name
         site_locale = os.path.join(settings.MULTI_TENANT_DIR, tenant_name, 'locale')
         tenant_name = connection.tenant.client_name
-        translation._trans._active.value = tenant_translation(language, tenant_name, site_locale)
+        
+        translation._trans._active.value = tenant_translation(self.language, tenant_name, site_locale)
 
-        try:
-            yield
-        finally:
-            translation.activate(prev)
-    else:
-        yield
+        return True
+
+    def __exit__(self, type, value, traceback):
+        translation.activate(self.prev)
+
+
+# @contextmanager
+# def tenant_language(language):
+#     """Context manager that changes the current translation language for
+#     all code inside the following block to the correct tenant specific language
+#     """
+
+#     if language:
+#         prev = translation.get_language()
+
+#         site_locale = os.path.join(settings.MULTI_TENANT_DIR, tenant_name, 'locale')
+#         tenant_name = connection.tenant.client_name
+#         translation._trans._active.value = tenant_translation(language, tenant_name, site_locale)
+
+#         try:
+#             yield
+#         finally:
+#             translation.activate(prev)
+#     else:
+#         yield
