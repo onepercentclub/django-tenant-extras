4008dc18fbd64bf821145ba040e9ecb51b6a93d3
John Vandenberg <jayvdb@gmail.com>
2020-10-03 20:00:58 +0700

    Update and test template loader

--- tenant_extras/template_loaders.py
+++ tenant_extras/template_loaders.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
-from django.template.exceptions import TemplateDoesNotExist
-from django.template.loaders.base import Loader
+from django.template.loaders.filesystem import Loader
 from django.utils._os import safe_join
 from django.db import connection
 
@@ -15,62 +14,21 @@ class FilesystemLoader(Loader):
     Changes are:
     - Use MULTI_TENANT_DIR from config for path (not multiple paths in MULTITENANT_TEMPLATE_DIRS)
     - Use tenant.client_name not tenant.domain_url
+    - Do not automatically include non-tenant filesystem template dirs
     """
+    def get_dirs(self):
+        if not connection.tenant or isinstance(connection.tenant, FakeTenant):
+            return []
 
-    is_usable = True
+        try:
+            template_dir = settings.MULTI_TENANT_DIR
+        except AttributeError:
+            raise ImproperlyConfigured('To use %s.%s you must define the MULTI_TENANT_DIR' %
+                                       (__name__, FilesystemLoader.__name__))
 
-    def get_template_sources(self, template_name, template_dirs=None):
-        if not connection.tenant:
-            return
-
-        # We can get here when the app errors before the tenant can
-        # be set on the connection, eg on 400 errors due to DEBUG = False
-        # and ALLOWED_HOSTS hasn't been correctly set.
-        elif isinstance(connection.tenant, FakeTenant):
-            if not template_dirs:
-                template_dirs = []
-            for template_dir in template_dirs:
-                try:
-                    yield safe_join(template_dir, template_name)
-                except UnicodeDecodeError:
-                    raise
-                except ValueError:
-                    pass
-            return
-
-        if not template_dirs:
-            try:
-                template_dirs = [settings.MULTI_TENANT_DIR]
-            except AttributeError:
-                raise ImproperlyConfigured('To use %s.%s you must define the MULTI_TENANT_DIR' %
-                                           (__name__, FilesystemLoader.__name__))
-
-        for template_dir in template_dirs:
-            try:
-                if '%s' in template_dir:
-                    yield safe_join(template_dir % connection.tenant.client_name, 'templates', template_name)
-                else:
-                    yield safe_join(template_dir, connection.tenant.client_name, 'templates', template_name)
-            except UnicodeDecodeError:
-                # The template dir name was a bytestring that wasn't valid UTF-8.
-                raise
-            except ValueError:
-                # The joined path was located outside of this particular
-                # template_dir (it might be inside another one, so this isn't
-                # fatal).
-                pass
-
-    def load_template_source(self, template_name, template_dirs=None):
-        tried = []
-        for filepath in self.get_template_sources(template_name, template_dirs):
-            try:
-                with open(filepath, 'rb') as fp:
-                    return (fp.read().decode(settings.FILE_CHARSET), filepath)
-            except IOError:
-                tried.append(filepath)
-        if tried:
-            error_msg = "Tried %s" % tried
+        if '%' in template_dir:
+            template_dir = safe_join(template_dir % connection.tenant.client_name, 'templates')
         else:
-            error_msg = "Your TEMPLATE_DIRS setting is empty. Change it to point to at least one template directory."
-        raise TemplateDoesNotExist(error_msg)
-    load_template_source.is_usable = True
+            template_dir = safe_join(template_dir, connection.tenant.client_name, 'templates')
+
+        return [template_dir]
